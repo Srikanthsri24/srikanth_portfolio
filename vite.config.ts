e9fa5a22027ec1aecdd1ea2903893743257876e6
Lalith Kumar <ippililalith@gmail.com>
2026-06-23 15:45:38 +0530

    fix custom domain github pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-const isGitHubPages = process.env.GITHUB_PAGES === "true";
-
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
-  base: isGitHubPages ? "/srikanth_portfolio/" : "/",
+  base: "/",
 });
