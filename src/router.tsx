57f66e4348fdf73b60f2b88a4bf69a5fb9adf89b
Lalith Kumar <ippililalith@gmail.com>
2026-06-23 14:33:47 +0530

    fix github pages basepath

--- src/router.tsx
+++ src/router.tsx
@@ -2,6 +2,11 @@ import { QueryClient } from "@tanstack/react-query";
 import { createRouter } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 
+const basepath =
+  import.meta.env.BASE_URL === "/"
+    ? "/"
+    : import.meta.env.BASE_URL.replace(/\/$/, "");
+
 export const getRouter = () => {
   const queryClient = new QueryClient();
 
@@ -10,6 +15,10 @@ export const getRouter = () => {
     context: { queryClient },
     scrollRestoration: true,
     defaultPreloadStaleTime: 0,
+
+    // Required for GitHub Pages sub-path:
+    // https://srikanthsri24.github.io/srikanth_portfolio/
+    basepath,
   });
 
   return router;
